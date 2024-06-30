cac006b9efa385586b32b710cf426d77016abf01
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-30 18:12:44 +0200

    [UPD] create-square-for-exercise-bonus

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 //CREO CICLO FOR DA 1 A 100//
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-    //CONTROLLO SE IL NUMERO E' MULTIPLO DI 3//
+    
+
+    //BONUS 1//
+   //RECUPERO ELEMENTO DEL DOM CONTENENTE I VALORI//
+   const grid = document.getElementById('grid');
+
+   //CREO UN ELEMENTO DA INSERIRE NEL DOM// 
+   const square = document.createElement('div');
+   square.classList.add('square');
+   console.log(square);
+    //CONTROLLO SE IL NUMERO E' MULTIPLO DI 3  E 5//
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('Fizz');
+        square.innerText = 'FizzBuzz';
+        square.classList.add('fizzbuzz');
+        console.log('FizzBuzz');
     }
     //CONTROLLO SE IL NUMERO E' MULTIPLO DI 5//
     else if (i % 5 == 0) {
+        square.innerText = 'Buzz';
+        square.classList.add('buzz');
         console.log('Buzz');
     }
-    //CONTROLLO SE IL NUMERO E' MULTIPLO DI 3 E 5//
+    //CONTROLLO SE IL NUMERO E' MULTIPLO DI 3//
     else if (i % 3 == 0) {
-        console.log('FizzBuzz');
+        square.innerText = 'Fizz';
+        square.classList.add('fizz');
+        console.log('Fizz');
     }
+    //SE NON E' STATA VERIFICATA NESSUNA CONDIZIONE STAMPO IL NUMERO EMERSO//
     else {
+        square.innerText = i;
+        
         console.log(i);
     }
+    //INSERISCO ELEMENTO SQUARE CREATO NELLA GRIGLIA TRAMITE FUNCTION APPEND//
+    grid.append(square);
 }
+
